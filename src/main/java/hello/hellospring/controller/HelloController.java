d4cd8e02f238bc005e3d3bf36c5c02b7295adf3f
dudtlstm <annpys1024@naver.com>
2023-07-02 17:14:19 +0900

    feat: [07/02] 섹션2 완료

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,12 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HelloController {
@@ -11,5 +15,38 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello"; // hello.html을 찾아서 렌더링하는 것
         // resources > templates 내의 html을 찾음
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // html에 나오는 body 태그 X. http의 body부에 해당 데이터를 넣을 것이라는 뜻
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // Getter Setter 사용
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
